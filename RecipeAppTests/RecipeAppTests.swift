c922be93c7d59a9e5fd9674d8002c19a487a156f
haoyang <yiphaoyang98@gmail.com>
2025-05-21 12:07:29 +0800

    Initial Commit

---
+++ RecipeAppTests/RecipeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppTests.swift
+//  RecipeAppTests
+//
+//  Created by Hao Yang Yip on 21/05/2025.
+//
+
+import Testing
+@testable import RecipeApp
+
+struct RecipeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
